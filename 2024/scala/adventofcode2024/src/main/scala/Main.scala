fa56cd3ed78862af33c1b0d23a66094c4ed90d7b
Oliver Tosky <olivertosky@gmail.com>
2024-12-06 10:26:41 -0500

    refactor parser from official solution

--- 2024/scala/adventofcode2024/src/main/scala/Main.scala
+++ 2024/scala/adventofcode2024/src/main/scala/Main.scala
@@ -1,18 +1,24 @@
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 import scala.util.Using
 
 
-def getDistanceBetweenLists(l1: List[Int], l2: List[Int]): Int =
-  l1.sorted().zip(l2.sorted()).map(_ - _).map(_.abs).sum
+def getDistanceBetweenLists(l1: Seq[Long], l2: Seq[Long]): Long =
+  l1.zip(l2).map(_ - _).map(_.abs).sum
+
+def parse(input: BufferedSource): (Seq[Long], Seq[Long]) =
+  val pairs = input
+    .getLines()
+    .map(line => line.split("   ").map(_.toLong))
+    .toSeq
+
+  val lefts = pairs.map(_.head).toSeq.sorted
+  val rights = pairs.map(_.last).toSeq.sorted
+  (lefts, rights)
 
 @main def solution(): Unit =
   val filePath = "/home/olivertosky/personal/advent-of-code/2024/scala/adventofcode2024/src/resources/inputs/day1.txt"
   Using(Source.fromFile(filePath)) { source =>
-    val parsed = for {
-      line <- source.getLines()
-      parts = line.split("\\s+") if parts.length >= 2
-    } yield (parts(0).toInt, parts(1).toInt)
-    val (l1, l2) = parsed.toList.unzip
-    println(getDistanceBetweenLists(l1, l2))
+    val (l1, l2) = parse(source)
+    println(getDistanceBetweenLists(l1, l2)) // 3714264
   }
 
