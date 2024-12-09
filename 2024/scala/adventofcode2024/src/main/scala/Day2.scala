116e0b2a2f431ac4a24f233ed674adddb99e19d7
Oliver Tosky <olivertosky@gmail.com>
2024-12-08 20:23:16 -0500

    typealias

--- 2024/scala/adventofcode2024/src/main/scala/Day2.scala
+++ 2024/scala/adventofcode2024/src/main/scala/Day2.scala
@@ -1,23 +1,25 @@
 import scala.io.{BufferedSource, Source}
 import scala.util.Using
 
-def parse(input: BufferedSource): Seq[Vector[Int]] =
+def parse(input: BufferedSource): Seq[Report] =
   input.getLines()
     .map(line => line.split(" ").map(_.toInt).toVector)
     .toSeq
 
-def isSafe(report: Seq[Int]): Boolean = {
+type Report = Seq[Int]
+
+def isSafe(report: Report): Boolean = {
   val diffs = report.sliding(2).map(pair => pair(1) - pair(0))
   if report.head < report.last then diffs.forall(d => 1 <= d && d <= 3) // ascending
   else if report.head > report.last then diffs.forall(d => -3 <= d && d <= -1) // descending
   else false
 }
-def numSafeReports(reports: Seq[Seq[Int]]): Int = reports.count(isSafe)
+def numSafeReports(reports: Seq[Report]): Int = reports.count(isSafe)
 
-def dampen(report: Seq[Int]): Seq[Seq[Int]] =
+def dampen(report: Report): Seq[Report] =
   report.to(LazyList).zipWithIndex.map((_, i) => report.patch(i, Nil, 1))
-def isAlmostSafe(report: Seq[Int]): Boolean = dampen(report).exists(isSafe)
-def numSafeDampenedReports(reports: Seq[Seq[Int]]): Int =
+def isAlmostSafe(report: Report): Boolean = dampen(report).exists(isSafe)
+def numSafeDampenedReports(reports: Seq[Report]): Int =
   reports.count(r => isSafe(r) || isAlmostSafe(r))
 
 @main def solutionDay2(): Unit = {
