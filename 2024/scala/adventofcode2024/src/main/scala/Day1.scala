c1d6e08161521d2de5574d9a878b1079d5c0c416
Oliver Tosky <olivertosky@gmail.com>
2024-12-08 20:36:28 -0500

    fmt

--- 2024/scala/adventofcode2024/src/main/scala/Day1.scala
+++ 2024/scala/adventofcode2024/src/main/scala/Day1.scala
@@ -1,36 +1,37 @@
 import scala.io.{BufferedSource, Source}
 import scala.util.Using
 
-
 def getDistance(l1: Seq[Long], l2: Seq[Long]): Long =
-  val distances = l1.zip(l2).map((left, right) => math.abs(left - right))
-  distances.sum
+    val distances = l1.zip(l2).map((left, right) => math.abs(left - right))
+    distances.sum
 
 def getSimilarity(l1: Seq[Long], l2: Seq[Long]): Long =
-  val countMap: Map[Long, Long] = l2.groupBy(identity).view.mapValues(_.size.toLong).toMap
+    val countMap: Map[Long, Long] =
+        l2.groupBy(identity).view.mapValues(_.size.toLong).toMap
 
-  val counts = l1.map(elem => countMap.getOrElse(elem, 0L) * elem)
-  counts.sum
-
-def parse(input: BufferedSource): (Seq[Long], Seq[Long]) =
-  val pairs = input
-    .getLines()
-    .map(line => line.split("   ").map(_.toLong))
-    .toSeq
-
-  val lefts = pairs.map(_.head).toSeq.sorted
-  val rights = pairs.map(_.last).toSeq.sorted
-  (lefts, rights)
+    val counts = l1.map(elem => countMap.getOrElse(elem, 0L) * elem)
+    counts.sum
 
 @main def solution(): Unit =
-  val filePath = "/home/olivertosky/personal/advent-of-code/2024/scala/adventofcode2024/src/resources/inputs/day1.txt"
-  Using(Source.fromFile(filePath)) { source =>
-    val (l1, l2) = parse(source)
+    def parse(input: BufferedSource): (Seq[Long], Seq[Long]) =
+        val pairs = input
+            .getLines()
+            .map(line => line.split("   ").map(_.toLong))
+            .toSeq
 
-    val distance = getDistance(l1, l2)
-    println(distance) // 3714264
+        val lefts = pairs.map(_.head).toSeq.sorted
+        val rights = pairs.map(_.last).toSeq.sorted
+        (lefts, rights)
 
-    val similarity = getSimilarity(l1, l2)
-    println(similarity) // 414
-  }
+    val filePath =
+        "/home/olivertosky/personal/advent-of-code/2024/scala/adventofcode2024/src/resources/inputs/day1.txt"
 
+    Using(Source.fromFile(filePath)) { source =>
+        val (l1, l2) = parse(source)
+
+        val distance = getDistance(l1, l2)
+        println(distance) // 3714264
+
+        val similarity = getSimilarity(l1, l2)
+        println(similarity) // 414
+    }
--- 2024/scala/adventofcode2024/src/test/scala/Solution2024D1.scala
+++ 2024/scala/adventofcode2024/src/test/scala/Solution2024D1.scala
@@ -1,12 +1,12 @@
 class Solution2024D1 extends munit.FunSuite {
-  test("distance solver") {
-    val l1 = List(3,4,2,1,3,3).map(_.toLong)
-    val l2 = List(4,3,5,3,9,3).map(_.toLong)
-    assertEquals(getDistance(l1, l2), 13L)
-  }
-  test("similarity solver") {
-    val l1 = List(3,4,2,1,3,3).map(_.toLong)
-    val l2 = List(4,3,5,3,9,3).map(_.toLong)
-    assertEquals(getSimilarity(l1, l2), 31L)
-  }
+    test("distance solver") {
+        val l1 = List(3, 4, 2, 1, 3, 3).map(_.toLong)
+        val l2 = List(4, 3, 5, 3, 9, 3).map(_.toLong)
+        assertEquals(getDistance(l1, l2), 13L)
+    }
+    test("similarity solver") {
+        val l1 = List(3, 4, 2, 1, 3, 3).map(_.toLong)
+        val l2 = List(4, 3, 5, 3, 9, 3).map(_.toLong)
+        assertEquals(getSimilarity(l1, l2), 31L)
+    }
 }
