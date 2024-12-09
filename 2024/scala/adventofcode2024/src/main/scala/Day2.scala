b76d779b7958f9eeec24608ad8408d2bbfb2b6b0
Oliver Tosky <olivertosky@gmail.com>
2024-12-08 20:07:17 -0500

    patch with Nil

--- 2024/scala/adventofcode2024/src/main/scala/Day2.scala
+++ 2024/scala/adventofcode2024/src/main/scala/Day2.scala
@@ -15,7 +15,7 @@ def isSafe(report: Vector[Int]): Boolean = {
 def numSafeReports(reports: Seq[Vector[Int]]): Int = reports.count(isSafe)
 
 def dampen(report: Vector[Int]): Seq[Vector[Int]] =
-  for i <- LazyList.from(report.indices) yield report.patch(i, Vector(), 1) // report.take(i) ++ report.drop(i + 1)
+  for i <- LazyList.from(report.indices) yield report.patch(i, Nil, 1) // report.take(i) ++ report.drop(i + 1)
 def isAlmostSafe(report: Vector[Int]): Boolean = dampen(report).exists(isSafe)
 def numSafeDampenedReports(reports: Seq[Vector[Int]]): Int =
   reports.count(r => isSafe(r) || isAlmostSafe(r))
