20c33a113f75974db8fab32af9221371302b6d49
Oliver Tosky <olivertosky@gmail.com>
2024-12-09 19:17:16 -0500

    add day 3

---
+++ 2024/scala/adventofcode2024/src/test/scala/Solution2024D3.scala
@@ -0,0 +1,14 @@
+class Solution2024D3 extends munit.FunSuite {
+    test("valid mult solver") {
+        val s = ")mul(1,100)where()how()mul(2,10);m"
+
+        assertEquals(part1(s), 100 + 20)
+    }
+
+    test("enabled mult solver") {
+        val s =
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+        assertEquals(part2(s), 2 * 4 + 8 * 5)
+    }
+}
